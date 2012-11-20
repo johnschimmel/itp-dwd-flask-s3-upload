ce01debddfe2cbed46f0a7afd4682416d4c932c9
John Schimmel <john.schimmel@gmail.com>
2012-11-20 06:48:49 -0500

    added PIL to requirements.txt

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import boto
 
 app = Flask(__name__)   # create our flask app
 app.secret_key = os.environ.get('SECRET_KEY') # put SECRET_KEY variable inside .env file with a random string of alphanumeric characters
-app.config['CSRF_ENABLED'] = True
+app.config['CSRF_ENABLED'] = False
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 megabyte file upload
 
 # --------- Database Connection ---------
@@ -32,18 +32,23 @@ def index():
 	# get Idea form from models.py
 	photo_upload_form = models.photo_upload_form(request.form)
 	
-	
 	# if form was submitted and it is valid...
 	if request.method == "POST" and photo_upload_form.validate():
-		file = request.files['fileupload']
-		app.logger.info(file)
-		app.logger.info(file.mimetype)
-		app.logger.info(dir(file))
+		uploaded_file = request.files['fileupload']
+		# app.logger.info(file)
+		# app.logger.info(file.mimetype)
+		# app.logger.info(dir(file))
 		
-		if file and allowed_file(file.filename):
+		# Uploading is fun
+		# 1 - Generate a file name with the datetime prefixing filename
+		# 2 - Connect to s3
+		# 3 - Get the s3 bucket, put the file
+		# 4 - After saving to s3, save data to database
+
+		if uploaded_file and allowed_file(uploaded_file.filename):
 			# create filename, prefixed with datetime
 			now = datetime.datetime.now()
-			filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(file.filename)
+			filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
 
 			# connect to s3
 			s3conn = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'),os.environ.get('AWS_SECRET_ACCESS_KEY'))
@@ -53,8 +58,8 @@ def index():
 			b = s3conn.get_bucket(os.environ.get('AWS_BUCKET')) # bucket name defined in .env
 			k = b.new_key(b)
 			k.key = filename
-			k.set_metadata("Content-Type", file.mimetype)
-			k.set_contents_from_string(file.stream.read())
+			k.set_metadata("Content-Type", uploaded_file.mimetype)
+			k.set_contents_from_string(uploaded_file.stream.read())
 			k.make_public()
 
 			if k and k.size > 0:
@@ -69,7 +74,7 @@ def index():
 
 			return filename
 		else:
-			return "uhoh there was an error " + file.filename
+			return "uhoh there was an error " + uploaded_file.filename
 
 
 
